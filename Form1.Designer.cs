f024e50a58652bb2127896a7f100e02135fe845f
근우 홍 <kalistar0524@gmail.com>
2026-04-02 11:47:37 +0900

    과제1 코딩과 테스트 완료 Listbox를 중앙에 Textbox를 ListBox 아래에 배치함 전송 버튼을 누르면 TextBox에 입력한 내용이 ListBox로 들어가 표시되고 TextBox의 내용은 깨끗하게 지워짐

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,18 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
+            txtID = new TextBox();
+            lblAppName = new Label();
+            txtPW = new TextBox();
+            btnLogin = new Button();
             SuspendLayout();
+            // 
+            // txtID
+            // 
+            txtID.Font = new Font("맑은 고딕", 24F);
+            txtID.ForeColor = Color.Silver;
+            txtID.Location = new Point(189, 227);
+            txtID.Multiline = true;
+            txtID.Name = "txtID";
+            txtID.Size = new Size(685, 98);
+            txtID.TabIndex = 2;
+            txtID.Text = "아이디";
+            txtID.Enter += txtID_Enter;
+            txtID.Leave += txtID_Leave;
+            // 
+            // lblAppName
+            // 
+            lblAppName.AutoSize = true;
+            lblAppName.Font = new Font("맑은 고딕", 40.125F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            lblAppName.Location = new Point(376, 34);
+            lblAppName.Name = "lblAppName";
+            lblAppName.Size = new Size(327, 142);
+            lblAppName.TabIndex = 0;
+            lblAppName.Text = "Login";
+            // 
+            // txtPW
+            // 
+            txtPW.Font = new Font("맑은 고딕", 24F);
+            txtPW.ForeColor = Color.Silver;
+            txtPW.Location = new Point(189, 396);
+            txtPW.Name = "txtPW";
+            txtPW.Size = new Size(685, 93);
+            txtPW.TabIndex = 3;
+            txtPW.Text = "패스워드";
+            txtPW.Enter += txtPW_Enter;
+            txtPW.Leave += txtPW_Leave;
+            // 
+            // btnLogin
+            // 
+            btnLogin.BackColor = Color.CornflowerBlue;
+            btnLogin.Font = new Font("맑은 고딕", 20F);
+            btnLogin.Location = new Point(376, 625);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(327, 111);
+            btnLogin.TabIndex = 1;
+            btnLogin.Text = "로그인";
+            btnLogin.UseVisualStyleBackColor = false;
+            btnLogin.Click += btnLogin_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(14F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1288, 789);
+            ClientSize = new Size(1065, 830);
+            Controls.Add(btnLogin);
+            Controls.Add(txtPW);
+            Controls.Add(lblAppName);
+            Controls.Add(txtID);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Login Sreen";
+            Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox txtID;
+        private Label lblAppName;
+        private TextBox txtPW;
+        private Button btnLogin;
     }
 }
